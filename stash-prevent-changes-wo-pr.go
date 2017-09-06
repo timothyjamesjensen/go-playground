5af956b6a92fc6a09e47ad42f5d4d58c872f9a8b
Tim Jensen <timothyjamesjensen@gmail.com>
2017-09-06 12:44:57 -0700

    Made baseUrl a command line variable

--- stash-prevent-changes-wo-pr.go
+++ stash-prevent-changes-wo-pr.go
@@ -1,4 +1,4 @@
-// Example use: go run stash-prevent-changes-wo-pr.go username password PIPE
+// Example use: go run stash-prevent-changes-wo-pr.go username password https://stash.americas.companyName.pvt/rest projectSlug
 
 package main
 
@@ -23,8 +23,8 @@ func main() {
 
 	username := os.Args[1]
 	password := os.Args[2]
-	project  := os.Args[3]
-	baseUrl := "https://stash.americas.nwea.pvt/rest"
+	baseUrl  := os.Args[3]
+	project  := os.Args[4]
 	config := []byte(`{
 		"id": "1",
 		"type": "pull-request-only",
@@ -55,7 +55,7 @@ func main() {
 	r := new(Repository)
 	json.NewDecoder(res.Body).Decode(&r)
 
-	for i:= 0; i < len(r.Values); i++ {
+	for i := 0; i < len(r.Values); i++ {
 		post, _ := http.NewRequest("POST", baseUrl + "/branch-permissions/2.0/projects/" + project + "/repos/" + r.Values[i].Slug  + "/restrictions", bytes.NewBuffer(config))
 		post.SetBasicAuth(username, password)
 		post.Header.Set("Content-Type", "application/json")
